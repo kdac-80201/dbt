022c5c0b9bfc72544cb094875aca150479709dde
mirzaAfnanBaig <mirzaafnanbaig98@gmail.com>
2023-09-09 17:24:03 +0530

    Assignment 1

--- print.c
+++ print.c
@@ -1,12 +1,3 @@
-#include<stdio.h>
-int main()
-{
-        for(int i =0;i<10;i++){
 
-            for(int j = 1;j<i;j++){
-               printf("* ");
-            }
-            printf("\n");
-        }
         
     }
